95dce366ac77841e5cb8c5e1ee26f98309558ec6
Raj Kumar <imraaj1026@gmail.com>
2025-08-03 09:57:20 +0530

    learn bash commands

---
+++ week2Offline/BashTerminal/app.js
@@ -0,0 +1,36 @@
+// Terminal
+
+// 1. pwd
+// 2. cd or cd ..
+// cd desktop/screenshots/today
+// use tab when u dont know the dir name
+
+// cd ../.. //to step back
+// 3. ls
+// 4. mkdir
+// 5. touch
+// 6. cat :- prints the inside content.
+// 7. vi :- used to edit file
+// 8. mv :- used to move file
+// mv test test-2/new-folder
+// 9. cp
+// cp -r test test2  # for copy folders
+// cp test.js test2  # for copy files
+// 10. nvm
+// 11. npm
+// 12. node
+// 13. git
+// 14. grep
+// 15. clear
+
+
+// terminal is nothing but another interface to do things
+// on your machine.
+
+
+// ls -l
+// ls -r
+// ls -t
+// ls -lt newdir
+// ls -la newdir
+// 
