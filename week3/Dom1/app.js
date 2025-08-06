8582423172affe53789b3defbf49d83f3f58ff27
Raj Kumar <imraaj1026@gmail.com>
2025-08-06 21:21:02 +0530

    Dom part 1 is completed

---
+++ week3/Dom1/app.js
@@ -0,0 +1,25 @@
+// There are 5 popular methods available for fetching DOM elements - 
+// querySelector
+// querySelectorAll
+// getElementById
+// getElementByClassName
+// getElementsByClassName
+
+
+// // 1. Fetching the title
+// const title = document.querySelector('h1');
+// console.log(title.innerHTML)
+
+// 2. Fetching the first TODO (Assignment)
+// const firstTodo = document.querySelector('h4');
+// console.log(firstTodo.innerHTML)
+
+// // 3. Fetching the second TODO (Assignment)
+// const secondTodo = document.querySelectorAll('h4')[1];
+// console.log(secondTodo.innerHTML)
+
+// // UPDATING ELEMENTS
+// const firstTodo = document.querySelector("h4");
+// firstTodo.innerHTML = "Dont' take class"
+
+
